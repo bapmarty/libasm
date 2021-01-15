8e3321d9fd3a347539fd41e7ae03ae0e248386d7
baptistemrt <contact@baptistemrt.fr>
2021-01-15 16:48:51 +0100

    :sparkles: Add ft_strlen function

---
+++ ft_strlen.s
@@ -0,0 +1,15 @@
+global _ft_strlen
+
+; size_t	ft_strlen(const char *s)
+
+_ft_strlen:
+	mov		rax, 0				; init rax to 0
+	
+loop:
+	cmp		BYTE [rdi + rax], 0	; compare address rdi and rax
+	je		exit				; rdi[rax] == 0; call exit func
+	inc		rax					; rax += 1
+	jmp		loop				; jump to loop
+
+exit:
+	ret							; return rax
---
+++ libasm.h
@@ -0,0 +1,12 @@
+#ifndef LIBASM
+# define LIBASM
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+ssize_t		ft_strlen(const char *s);
+
+#endif
