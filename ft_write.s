b908d863761d01d30e6fb7b0861187d83cd48748
baptistemrt <contact@baptistemrt.fr>
2021-01-16 01:30:24 +0100

    :sparkles: Add ft_write function

---
+++ ft_write.s
@@ -0,0 +1,13 @@
+global _ft_write
+
+; ft_write(rdi = [argv[0]], rsi = [argv[1]], rdx = [argv[2]])
+
+_ft_write:
+	mov		rax, 0x2000004	; Move sys_write in rax
+	syscall
+	jc		err_exit		; Exit if err
+	ret						; Return length of string
+
+err_exit:
+	mov		rax, -1			; Set rax with value -1
+	ret						; Return rax error
--- libasm.h
+++ libasm.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 
 ssize_t		ft_strlen(const char *s);
+ssize_t		ft_write(int fildes, const char *buf, size_t nbyte);
 
 int			ft_strcmp(const char *s1, const char *s2);
 
