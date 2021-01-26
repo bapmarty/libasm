2d0b7c28b251efdb2f02de40a0b262da58783b46
baptistemrt <contact@baptistemrt.fr>
2021-01-26 13:20:51 +0100

    :bug: Fix ft_strcmp for macos

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -3,24 +3,40 @@ global _ft_strcmp
 ; int	strcmp(const char *s1, const char *s2)
 
 _ft_strcmp:
-	mov		al, BYTE [rdi]	; Move first byte of rdi in al
-	mov		bl, BYTE [rsi]	; Move first byte of rsi in bl
+	push	rbp						; https://beta.hackndo.com/stack-introduction/#prologue---%C3%A9pilogue
+	mov		rbp, rsp				; https://stackoverflow.com/questions/42208087/are-the-prologue-and-epilogue-mandatory-when-writing-assembly-functions
+	xor		rax, rax                ; Init rax to 0
+	xor		rbx, rbx                ; Init iterative to 0
+	xor		cx, cx                  ; Init cx to 0 to manipulate rdi
+	xor		dx, dx                  ; Init dx to 0 to manipulate rsi
 
-empty_string:
-	cmp		al, 0			; Compare if al == eos
-	je		exit			; Exit
-	cmp		bl, 0			; Compare if al == eos
-	je		exit			; Exit
+_loop:
+	mov		cl, BYTE [rdi + rbx]	; Move byte of rdi in cl
+	mov		dl, BYTE [rsi + rbx]	; Move byte of rsi in bl
+	or		cl, cl					; return if '\0' in first string ; https://stackoverflow.com/questions/22123863/or-al-al-in-x86-assembly
+	jz		_exit						
+	or		dl, dl					; return if '\0' in second string
+	jz		_exit
+	cmp		cl, dl					; Exit func if strings cl, dl not equals
+	jne		_exit					
+	inc		rbx 					; Increment iterator
+	jmp		_loop					; Jump to _loop to continue the string comparaison
 
-compare_string:
-	cmp		al, bl			; Compare strings al and bl
-	jne		exit			; If not equal exit
-	inc		rdi				; Increment rdi
-	inc		rsi				; Increment rsi
-	jmp		_ft_strcmp		; Jump to _ft_strcmp to continue the string comparaison
+_exit:
+	sub		cx, dx					; subtraction of char cx, dx
+	jl		_neg					; if neg ret -1
+	jg		_pos					; if pos ret 1
+	mov		rax, 0					; if equal to 0 ret 0
+	pop		rbp
+	ret								; ret rax
 
-exit:
-	movzx	rax, al			; Move value al in rax
-	movzx	r8, bl			; Move value bl in r8
-	sub		rax, r8			; rax - r8 and obtain the difference
-	ret						; Return rax
+_pos:
+	mov		rax, 1					; set rax to 1
+	pop		rbp
+	ret
+
+_neg:
+	mov 	rax, -1					; set rax to -1
+	pop rbp
+	ret
+
